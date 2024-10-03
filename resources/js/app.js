e5b209e1b98eab52ec8076a64a89ae89357865eb
Alex Krol <krolmail@list.ru>
2024-10-03 23:44:42 +0600

    fix, delete my layout solution

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,11 @@ import '~/css/app.css';
 import './bootstrap';
 import { createInertiaApp } from '@inertiajs/svelte';
 
-import Layout from './core/Layout.svelte';
-
 createInertiaApp({
 	resolve: (name) => {
 		const pages = import.meta.glob('./pages/**/*.svelte', { eager: true });
 		const page = pages[`./pages/${name}.svelte`];
-		return { default: page.default, layout: Layout };
+		return { default: page.default };
 	},
 	setup({ el, App, props }) {
 		new App({ target: el, props });
--- resources/js/layouts/Guest.svelte
+++ resources/js/layouts/Guest.svelte
@@ -3,16 +3,14 @@
 	import { Link } from '@inertiajs/svelte';
 </script>
 
-<!-- sm:justify-center sm:pt-0 -->
-<div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6">
+<div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
 	<div>
 		<Link href="/">
 			<BreezeApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
 		</Link>
 	</div>
 
-	<!-- sm:max-w-md sm:rounded-lg -->
-	<div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md">
+	<div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
 		<slot />
 	</div>
 </div>
